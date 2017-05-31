76e12f20c25679567246a34e7487b89204380ed8
Aidan Twomey <aidantwomey@Aidans-MacBook-Pro.local>
2017-05-31 22:40:37 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace HelloCloudSql
+{
+    class Program
+    {
+        const string sql_connection = "Server=tcp:aidan-mssql.c9i3yi2murx6.eu-west-1.rds.amazonaws.com,1433;Initial Catalog=musicDownloads;User Id=aidan2mey;Password=lalaland;";
+        static void Main(string[] args)
+        {
+            using (var connection = new SqlConnection(sql_connection))
+            {
+                var command = new SqlCommand("select * from product", connection);
+                connection.Open();
+                
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader[0]}:{reader[1]}");
+                    }
+                }
+            }
+        }
+    }
+}
